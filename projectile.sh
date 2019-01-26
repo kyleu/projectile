9f91ae6a5bb154f5ed6e8cf016bf469a308d6d86
Kyle Unverferth <kyle@kyleu.com>
2019-01-26 11:27:26 -0700

    Startup script that downloads and runs the latest version

---
+++ projectile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+working_dir=${pwd}
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -f ${script_dir}/projectile.jar ]; then
+  echo "Downloading latest projectile release..."
+  cd $script_dir
+  curl -s https://api.github.com/repos/KyleU/projectile/releases/latest | grep "browser_download_url.*jar" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+  cd $working_dir
+  echo "Finished download"
+fi
+
+# Run jar using original working directory
+java -jar "${script_dir}/projectile.jar" $@
